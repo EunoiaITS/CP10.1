01e0365fb82a04daa79590e987047f4deed1da0f
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-06-06 13:17:05 +0600

    update main.js

--- webroot/assets/js/main.js
+++ webroot/assets/js/main.js
@@ -28,26 +28,28 @@
         Datepicker init
       =========================================*/
 
-      $('.datepicker-f').datetimepicker({
-        format: "YYYY-MM-DD",
-        icons: {
-          up: 'fa fa-angle-up',
-          down: 'fa fa-angle-down',
-          previous: 'fa fa-angle-left',
-          next: 'fa fa-angle-right',
-        }
-      });
+      $('.datepicker-f').datepicker({
+            format: 'yyyy-dd-mm',
+            todayHighlight: true,
+            icons: {
+              up: 'fa fa-angle-up',
+              down: 'fa fa-angle-down',
+              previous: 'fa fa-angle-left',
+              next: 'fa fa-angle-right',
+            }
+        });
 
       /*=======================================
         Timepicker init 
       =========================================*/
 
-        $('.timepicker-f').datetimepicker({
-        format: "HH:mm A",
-        icons: {
-          up: 'fa fa-angle-up',
-          down: 'fa fa-angle-down',
-          previous: 'fa fa-angle-left',
-          next: 'fa fa-angle-right',
-        }
-      });
+        $('.timepicker-f').datepicker({
+            format: 'yyyy-dd-mm',
+            todayHighlight: true,
+            icons: {
+              up: 'fa fa-angle-up',
+              down: 'fa fa-angle-down',
+              previous: 'fa fa-angle-left',
+              next: 'fa fa-angle-right',
+            }
+        });
